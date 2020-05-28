28fc7ecd9a54324ec58ef207c91b77c125892957
dfsd534 <dfsd534@qq.com>
2020-05-28 22:27:49 +0800

    111

--- timedemo/main.go
+++ timedemo/main.go
@@ -5,8 +5,19 @@ import (
 	"time"
 )
 
-func main () {
+func main() {
 	now := time.Now()
 
 	fmt.Println(now.Format("2006-01-02 15:04:05"))
+	fmt.Println(now.Format("3"))
+
+	i := 1
+	for {
+		i++
+		time.Sleep(time.Second)
+		fmt.Println(i)
+		if i > 10 {
+			break
+		}
+	}
 }
